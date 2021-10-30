6a0e8c27e4c68beebfaf71da617d5159c0f17b86
Tony <tony.li.email@gmail.com>
2021-10-30 17:19:29 -0400

    Basically functional.

--- src/App.js
+++ src/App.js
@@ -5,25 +5,83 @@ import React from 'react'
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { Container, Navbar } from 'react-bootstrap';
+import { Container, ListGroup, Navbar } from 'react-bootstrap';
+
+function getMarginCallPrice(price, initial_margin, maintenance_margin, type, callback) {
+    //fetch('http://localhost:5000/graphql', {
+    fetch('https://merger-arb.herokuapp.com/graphql', {
+    method: "POST",
+    headers: { // Note: This is headers with a 's'. 
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ // Note: Query starts and end with backtick!
+      query: 
+      `mutation { 
+        marginCallPrice(
+          price: ${price} 
+          initial_margin: ${initial_margin} 
+          maintenance_margin: ${maintenance_margin} 
+          type: ${type}
+        ) 
+        {
+          id 
+          price 
+          initial_margin 
+          maintenance_margin 
+          type 
+          margin_call_price 
+        } 
+      }`
+    })
+  })
+    .then(res => res.json())
+    .then(res => callback(res.data))
+    .catch(console.error)
+}
 
 class InputField extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      price: 0.0,
+      initial_margin: 0.0,
+      maintenance_margin: 0.0,
+      type: "LONG",
+      price_history : []
+    };
+
     this.afterSubmission = this.afterSubmission.bind(this)
   }
 
   afterSubmission = (event) => {
     event.preventDefault();
+
+    getMarginCallPrice(this.state.price, this.state.initial_margin, this.state.maintenance_margin, this.state.type, ( {marginCallPrice} ) => {
+      if (marginCallPrice) {
+
+        this.setState( prevState => ({
+          price_history: [...prevState.price_history, marginCallPrice]
+        }));
+
+      }
+    });
   }
 
   render() {
     return(
-      <Form onSubmit = {this.afterSubmission}>
+      <Container fluid>
+        <Form className="mb-3" onSubmit = {this.afterSubmission}>
           <Form.Group className="mb-3">
             <Form.Label>Price:</Form.Label> 
-            <Form.Control type="number" step="any" placeholder="Amount in dollars" />
+            <Form.Control type="number" step="any" placeholder="Amount in dollars" 
+              onChange={event => {
+                this.setState({
+                  price: event.target.value
+                });
+                }
+              } 
+            />
             <Form.Text>
               Please enter a non-negative value.
             </Form.Text>
@@ -31,7 +89,14 @@ class InputField extends React.Component {
           
           <Form.Group className="mb-3">
             <Form.Label>Initial Margin:</Form.Label> 
-            <Form.Control type="number" step="any" placeholder="Decimal value between 0 and 1" />
+            <Form.Control type="number" step="any" placeholder="Decimal value between 0 and 1"
+              onChange={event => {
+                this.setState({
+                  initial_margin: event.target.value
+                });
+                }
+              } 
+            />
             <Form.Text>
               e.g. "0.5" implies a initial margin of 50%
             </Form.Text>
@@ -39,7 +104,14 @@ class InputField extends React.Component {
 
           <Form.Group className="mb-3">
             <Form.Label>Maintenance Margin:</Form.Label> 
-            <Form.Control type="number" step="any" placeholder="Decimal value between 0 and 1" />
+            <Form.Control type="number" step="any" placeholder="Decimal value between 0 and 1" 
+              onChange={event => {
+                this.setState({
+                  maintenance_margin: event.target.value
+                });
+                }
+              } 
+            />
             <Form.Text>
               Note: Maintenance margin cannot be higher than initial margin.
             </Form.Text>
@@ -47,18 +119,37 @@ class InputField extends React.Component {
           
           <Form.Group className="mb-3">
             <Form.Label>Type:</Form.Label>
-            <Form.Control as="select">
+            <Form.Control as="select" 
+              onChange={event => {
+                this.setState({
+                  type: event.target.value
+                });
+                }
+              } 
+            >
               <option value="LONG">Long</option>
               <option value="SHORT">Short</option>
             </Form.Control>  
           </Form.Group>
 
           <Form.Group className="mb-6">
-            <Button variant="primary" type="submit">
+            <Button variant="primary" type="submit" >
               Submit
             </Button>
           </Form.Group>
         </Form>
+
+        <ListGroup className="mb-3">
+          {
+            this.state.price_history.reverse().map( (item) => (
+              <ListGroup.Item id={item.id} >
+                Margin Call Price: ${item.margin_call_price.toFixed(2)}
+              </ListGroup.Item>
+            ))
+          }
+        </ListGroup>
+
+      </Container>
     )
   }
 }
@@ -75,10 +166,8 @@ function App() {
         </Container>
       </Navbar>
       
-      <Container fluid>
-        <InputField>
-        </InputField>
-      </Container>
+      <InputField>
+      </InputField>
     </div>
   );
 }
